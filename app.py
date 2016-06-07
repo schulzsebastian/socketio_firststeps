04a920952ce7da2fbc3f7266603eb2d50562e812
schulzsebastian <schulz.siwy@gmail.com>
2016-06-07 23:57:58 +0200

    circle with accuracy

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask.ext.socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'xxxx'
