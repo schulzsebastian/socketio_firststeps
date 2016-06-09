fe2b185b084d1221ff469c06d6d6e28020020d76
Sebastian Schulz <schulz.siwy@gmail.com>
2016-06-09 11:51:50 +0200

    chat feature

--- app.py
+++ app.py
@@ -11,8 +11,13 @@ def index():
 
 @socketio.on('location', namespace='/broadcast')
 def location(message):
-    emit('locations', {'username': message['username'], 'coords': message['coords'], 'acc': message['acc']}, broadcast=True)
+    emit('location', {'username': message['username'], 'coords': message['coords'], 'acc': message['acc']}, broadcast=True)
     print str({'username': message['username'], 'coords': message['coords'], 'acc': message['acc']})
+
+@socketio.on('message', namespace='/broadcast')
+def location(message):
+    emit('message', {'username': message['username'], 'message': message['message']}, broadcast=True)
+    print str({'username': message['username'], 'message': message['message']})
 
 @socketio.on('server_connect', namespace='/broadcast')
 def server_connect(message):
@@ -20,5 +25,10 @@ def server_connect(message):
     emit('server_connect', {'username': message['username']}, broadcast=True)
     print 'User '+message['username']+' logged in.'
 
+@socketio.on('disconnect', namespace='/broadcast')
+def disconnect():
+    emit('disconnect', {'username': session['username']}, broadcast=True)
+    print 'User '+session['username']+' disconnected.'
+
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5005)
+    socketio.run(app)
